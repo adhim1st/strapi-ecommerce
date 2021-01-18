f77fde578433b6403f3ae38673013c8e37e4a940
adhim1st <adhim@student.uns.ac.id>
2021-01-18 18:12:15 +0700

    strapi ecommerce server

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3c789aff977e5745c4029e037568aeb9'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '080c06e5-9954-48c6-b00b-8f6197f4bd72'
+};
